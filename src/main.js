f41502e9cbfd3d5f6259876d527e4e99be9554e0
YingXiaoMo <yingxiaomoya@gmail.com>
2025-12-06 21:13:43 +0800

    feat: install popular iconify collections (mdi, ph, tabler, hugeicons, carbon)

--- src/main.js
+++ src/main.js
@@ -3,19 +3,38 @@ import { createPinia } from 'pinia';
 import { createI18n } from 'vue-i18n';
 import App from './App.vue';
 import { addCollection } from '@iconify/vue';
+
+// Import Icon Collections
 import riIcons from '@iconify-json/ri/icons.json';
 import mingcuteIcons from '@iconify-json/mingcute/icons.json';
+import mdiIcons from '@iconify-json/mdi/icons.json';
+import phIcons from '@iconify-json/ph/icons.json';
+import tablerIcons from '@iconify-json/tabler/icons.json';
+import hugeiconsIcons from '@iconify-json/hugeicons/icons.json';
+import carbonIcons from '@iconify-json/carbon/icons.json';
+
 import { initConsole } from '@/utils/console';
 import zh from '@/locales/zh.json';
 import en from '@/locales/en.json';
+
 if (!window.ColorThief) {
   window.ColorThief = class {
     getColor() { return [0, 0, 0]; }
     getPalette() { return [[0, 0, 0]]; }
   };
 }
-addCollection(riIcons);
-addCollection(mingcuteIcons);
+
+// Register all icon collections
+[
+  riIcons, 
+  mingcuteIcons, 
+  mdiIcons, 
+  phIcons, 
+  tablerIcons, 
+  hugeiconsIcons, 
+  carbonIcons
+].forEach(addCollection);
+
 const i18n = createI18n({
   legacy: false, 
   locale: localStorage.getItem('lang') || 'zh', 
@@ -25,11 +44,15 @@ const i18n = createI18n({
     en
   }
 });
+
 import router from './router';
+
 const app = createApp(App);
 const pinia = createPinia();
+
 app.use(pinia);
 app.use(router);
 app.use(i18n); 
 app.mount('#app');
+
 initConsole();
